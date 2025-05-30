09ae7552e87e0a830e47bc5e64d54935fa0cd36f
Henry Ward <45144290+kudosscience@users.noreply.github.com>
2025-05-30 13:54:41 +0100

    Add initial views for HelloWorld and Welcome pages
    - Created Index.cshtml for displaying a movie list with a greeting message.
    - Created Welcome.cshtml to display a welcome message multiple times based on ViewData.
    - Added sourcelink configuration for debugging support.

--- obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ae44dacb6d373842e05fa3bcca65dfa672abbe7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
